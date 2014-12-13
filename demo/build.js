38fbb5e866a3b80041364407a861959da697ceb1
Tiddo Langerak <tiddolangerak@gmail.com>
2014-12-13 17:34:08 +0100

    Fully working loader

--- demo/build.js
+++ demo/build.js
@@ -2,12 +2,16 @@
 
 var b = require('browserify');
 var through = require('through2');
-var turboloader = require('../turboloader');
+//var turboloader = require('../turboloader');
 var fs = require('fs');
+var bpack = require('browser-pack');
+var xtend = require('xtend');
+var util = require('util');
 process.chdir(__dirname);
 
 //We'll have to pass the turboloader an upload url and a current version number
 var options = {
+	full : '/full.json',
 	version : 1,
 	replacements : '/changes.json?since=%v',
 	alwaysReplace : true
@@ -23,9 +27,11 @@ var options = {
 //- Automatically generate version diffs
 
 //TODO: fix inconsistent naming
-b('./index.js', { debug : true, exposeAll: true, prelude: turboloader.createPrelude(options) })
+b('./index.js', { debug : true , exposeAll : true })
 .plugin(function(bundle, opts) {
-//	console.log(bundle.pipeline.get('pack'))
+	bundle._options.prelude = "Array";
+	bundle._options.exposeAll = true;
+	bundle.pipeline.get('pack').splice(0, 1, bpack(xtend(bundle._options, { raw : true })));
 	/*
 	bundle.pipeline.get('json').unshift(through.obj(function(row, enc, next) {
 		console.log("json");
@@ -61,8 +67,31 @@ b('./index.js', { debug : true, exposeAll: true, prelude: turboloader.createPrel
 
 
 })
-.bundle()
-.pipe(fs.createWriteStream(__dirname + "/out.js"))
+.bundle(function(err, buff) {
+	if (err) throw err;
+
+	var bundleString = buff.toString().trim();
+	var jsonBundle = bundleStringToJson(bundleString);
+	fs.writeFileSync(__dirname + "/full.json", JSON.stringify(jsonBundle, null, 4), "utf8");
+})
+//.pipe(fs.createWriteStream(__dirname + "/full.json"))
 .on('error', function(err) {
 	throw err;
 });
+
+function bundleStringToJson(moduleString) {
+	var preludeArgs = eval(moduleString);
+	var modules = preludeArgs[0];
+	var serializedModules = {};
+	//Map all modules to a serialized format.
+	for (var name in modules) {
+		var module = modules[name];
+		serializedModules[name] = [module[0].toString(), module[1]];
+	}
+	var bundle = {
+		version : 1,
+		modules : serializedModules,
+		entry : preludeArgs[2]
+	};
+	return bundle;
+}
--- turboloader.js
+++ turboloader.js
@@ -3,10 +3,18 @@
  * This is a wrapper around the browserify-prelude function with support for intelligent updating of scripts.
  * It keeps track of the version already downloaded to the user's pc (localstorage) and explicitely
  * requests the server for a changeset. It will then update it's module cache with the changes.
+ *
+ * TODOs:
+ * - [ ] If localstorage fails then fallback to just using the downloaded full bundle.
+ * - [ ] If diff fails download latest instead (configurable?)
+ * - [ ] If diff fails & cannot download latest, use from cache (configurable?)
+ * - [ ] Version checking: let diff echo back the base version such that we can verify we received
+ *       the correct diff.
+ * - [ ] Expose a function that enables the user code to invalidate the cache.
  */
-//TODO: cleaner (de)serialization
 function turboloader(opts, modules, cache, entry) {
-
+	opts.storage = opts.storage || window.localStorage;
+	opts.storageKey = opts.storageKey || '__bundle';
 	//--START IMPORT PRELUDE-- TODO: don't copy-paste
 	// Save the require from previous bundle to this closure if any
 	function outer (modules, cache, entry) {
@@ -38,68 +46,145 @@ function turboloader(opts, modules, cache, entry) {
 	}
 	//---END IMPORT PRELUDE---
 
-	opts.storage = opts.storage || window.localStorage;
-	opts.version = Number(opts.storage.getItem('__v') || opts.version);
-	var storedModules = opts.storage.getItem('__modules');
-	if (storedModules) {
-		modules = JSON.parse(storedModules);
-		for (var moduleName in modules) {
-			var functionString = modules[moduleName][0];
-			modules[moduleName][0] = eval('(' + functionString + ')');
+
+	var functionMatcher = /^function(?:.*?)\((.*?)\)\s*{((?:.|\n)*)}$/m;
+	/**
+	 * This function deserializes a module function such that it can both be run and debugged.
+	 *
+	 * @param {String} moduleString - The string containing the module function.
+	 * @param {String} name - The name of the module
+	 * @return {Function} The module function.
+	 */
+	function deserializeModuleFunction(moduleString, name) {
+		var sourceURL = window.location.href + name;
+		sourceURL = sourceURL.replace(/\/+/g, '/');
+
+		//We could use eval to quickly evaluate the module, but that's a bit unsafe. Therefore we
+		//construct a function instead. Still unsafe, but a bit less than eval.
+		//This does require the modulestring to obey a few constraints:
+		//- The module must be exactly one function.
+		//- The function cannot be enclosed in anything. That is, the string must begin with
+		//  "function(<params>){" and end with "}"
+		//- The function signature cannot contain any newlines (leading is ok, that's trimmed)
+		moduleString = moduleString.trim();
+		var matches = moduleString.match(functionMatcher);
+		var params = matches[1].split(",").map(function(param) {
+			return param.trim();
+		});
+		var body = matches[2].trim();
+		body += "\n\/\/# sourceURL=" + sourceURL;
+		return new Function(params, body);
+	}
+
+	/**
+	 * Deserialize an entire bundle
+	 *
+	 * @param {String} bundle - A JSON bundle string (e.g. from cache)
+	 * @return {Object} A deserialized bundle object.
+	 */
+	function deserializeBundle(bundle) {
+		bundle = JSON.parse(bundle);
+		var deserializedModules = {};
+		for (var name in bundle.modules) {
+			var module = bundle.modules[name];
+			var moduleFunction = deserializeModuleFunction(module[0], name);
+			var moduleDeps = module[1];
+			deserializedModules[name] = [ moduleFunction, moduleDeps ];
 		}
+		bundle.modules = deserializedModules;
+		return bundle;
 	}
 
-	function createModuleFunction(moduleString, fileName) {
-		fileName = window.location.href + fileName;
-		fileName = fileName.replace(/\/+/g, '/');
-		var expression = ["(function(require,module,exports){",
-			"",
-			moduleString,
-			"",
-			"})",
-			"\/\/# sourceURL=" + fileName
-		].join('\n');
-		return eval(expression);
-	}
-
-
-	//Do a request for the changeset
-	var url = opts.replacements.replace('%v', opts.version);
-	var feyenoord = new XMLHttpRequest();
-	feyenoord.addEventListener('readystatechange', function() {
-		if (feyenoord.readyState === 4) {
-			if (feyenoord.status === 200) {
-				var diff = JSON.parse(feyenoord.responseText);
-				if (!opts.alwaysReplace && diff.version === opts.version) {
-					console.log('Already having the most up to date version');
+	/**
+	 * Fetches a bundle.
+	 *
+	 * @param {String} url - The url to fetch the bundle from
+	 * @param {(String, Object) -> any} - A node-style callback function that will be called with the
+	 *                                    (deserialized) bundle on success.
+	 */
+	function fetchBundle(url, cb) {
+		var feyenoord = new XMLHttpRequest();
+		feyenoord.addEventListener('readystatechange', function() {
+			if (feyenoord.readyState === 4) {
+				if (feyenoord.status === 200) {
+					var modules = deserializeBundle(feyenoord.responseText);
+					cb(null, modules);
 				} else {
-					for (var moduleName in diff.modules) {
-						var module = diff.modules[moduleName];
-						var moduleFunction = createModuleFunction(module[0], moduleName);
-						var mDeps = module[1];
-						modules[moduleName] = [
-							moduleFunction,
-							mDeps
-						];
-					}
-					opts.storage.setItem('__v', diff.version);
-					opts.storage.setItem('__modules', JSON.stringify(modules, function serializer(key, value) {
-						if (typeof value === 'function') {
-							return value.toString();
-						}
-						return value;
-					}));
+					cb(new Error("Could not fetch bundle. Status code: " + feyenoord.status));
 				}
-				outer(modules, cache, entry);
 			}
-		}
-	});
-	feyenoord.open('GET', url, true);
-	feyenoord.send();
+		});
+		feyenoord.open('GET', url, true);
+		feyenoord.send();
+	}
 
+	/**
+	 * Updates the cache with a new bundle.
+	 *
+	 * @param {Object} bundle - The bundle to store in cache
+	 */
+	function updateCache(bundle) {
+		opts.storage.setItem(opts.storageKey, JSON.stringify(bundle, function serializer(key, value) {
+			if (typeof value === 'function') {
+				//remove sourceURL comments and the like. Not doing so would result in more and more comments
+				//being added each time the cache is updated
+				return  value.toString().replace(/\/\/\#.*$/mg, '');
+			}
+			return value;
+		}));
+	}
+
+	/**
+	 * Updates a bundle by fetching the diff from the server and applying it against the base bundle.
+	 *
+	 * @param {Object} opts - The options for the loader.
+	 * @param {Object} bundle - A base bundle
+	 * @param {(String, Object) -> any} cb - A node style callback function that will be called with
+	 *                                       the new bundle on success.
+	 */
+	function updateBundle(opts, bundle, cb) {
+		var url = opts.replacements.replace('%v', bundle.version);
+		fetchBundle(url, function(err, diff) {
+			if (err) return cb(err);
+			bundle.version = diff.version || bundle.version;
+			bundle.entry = diff.entry || bundle.entry;
+			for (var name in diff.modules) {
+				bundle.modules[name] = diff.modules[name];
+			}
+			cb(null, bundle);
+		});
+	}
+
+	/**
+	 * Starts the application contained in the bundle.
+	 * @param {Object} bundle - The bundle to launch.
+	 */
+	function start(bundle) {
+		outer(bundle.modules, {}, bundle.entry);
+	}
+
+	/**
+	 * Initializes the entire application.
+	 * It fetches the latest version, applies diffs when necessary, and then starts the application.
+	 */
+	function initialize() {
+		var bundle = opts.storage.getItem(opts.storageKey);
+		if (bundle) {
+			bundle = deserializeBundle(bundle);
+			updateBundle(opts, bundle, function(err, bundle) {
+				if (err) throw err;
+				updateCache(bundle);
+				start(bundle);
+			});
+		} else {
+			fetchBundle(opts.full, function(err, bundle) {
+				if (err) throw err;
+				updateCache(bundle);
+				start(bundle);
+			});
+		}
+	}
+
+	initialize();
 }
 
-//Returns a ready made prelude function.
-module.exports.createPrelude = function(opts) {
-	return '(' + turboloader.toString() + '.bind(null, ' + JSON.stringify(opts) + '))';
-};
